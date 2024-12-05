1f79832600cf440f63f3de5c5decc5af8edbff4b
beatrice <wairimubeatrice209@gmail.com>
2024-12-05 22:55:57 +0300

    It was hard

--- Net-Salary-Calculator/net.js
+++ Net-Salary-Calculator/net.js
@@ -3,56 +3,55 @@ function calculateNetSalary(basicSalary, benefits) {
 
     // PAYE Calculations
     let paye;
-    if (grossSalary <= 24000) {
+    if (grossSalary <= 24000) 
         paye = grossSalary * 0.1;
-    } else if (grossSalary <= 32333) {
-        paye = grossSalary * 0.25;
-    } else if (grossSalary <= 500000) {
+    else if (grossSalary <= 32333) 
+      paye = grossSalary * 0.25;
+    else if (grossSalary <= 500000) 
         paye = grossSalary * 0.3;
-    } else if (grossSalary <= 800000) {
+     else if (grossSalary <= 800000) 
         paye = grossSalary * 0.325;
-    } else {
+     else 
         paye = grossSalary * 0.35;
-    }
+    
 
     // NHIF Deductions
     let nhif;
-    if (grossSalary <= 5999) {
+    if (grossSalary <= 5999) 
         nhif = 150;
-    } else if (grossSalary <= 7999) {
+     else if (grossSalary <= 7999) 
         nhif = 300;
-    } else if (grossSalary <= 11999) {
+     else if (grossSalary <= 11999) 
         nhif = 400;
-    } else if (grossSalary <= 14999) {
+     else if (grossSalary <= 14999) 
         nhif = 500;
-    } else if (grossSalary <= 19999) {
+     else if (grossSalary <= 19999) 
         nhif = 600;
-    } else if (grossSalary <= 24999) {
+    else if (grossSalary <= 24999) 
         nhif = 750;
-    } else if (grossSalary <= 29999) {
+     else if (grossSalary <= 29999) 
         nhif = 850;
-    } else if (grossSalary <= 34999) {
+     else if (grossSalary <= 34999) 
         nhif = 900;
-    } else if (grossSalary <= 39999) {
+    else if (grossSalary <= 39999) 
         nhif = 950;
-    } else if (grossSalary <= 44999) {
+    else if (grossSalary <= 44999) 
         nhif = 1000;
-    } else if (grossSalary <= 49999) {
+    else if (grossSalary <= 49999) 
         nhif = 1100;
-    } else if (grossSalary <= 59999) {
+    else if (grossSalary <= 59999) 
         nhif = 1200;
-    } else if (grossSalary <= 69999) {
+    else if (grossSalary <= 69999) 
         nhif = 1300;
-    } else if (grossSalary <= 79999) {
+    else if (grossSalary <= 79999) 
         nhif = 1400;
-    } else if (grossSalary <= 89999) {
+     else if (grossSalary <= 89999) 
         nhif = 1500;
-    } else if (grossSalary <= 99999) {
+     else if (grossSalary <= 99999) 
         nhif = 1600;
-    } else {
+     else 
         nhif = 1700;
-    }
-
+    
     // NSSF Deductions
     let nssf;
     let tier1Limit = 7000;
--- Speed-Detector/speed.js
+++ Speed-Detector/speed.js
@@ -1,18 +1,25 @@
 
-function radarDevice(speed) { //This function is called radar device.
-  const velocityLimit = 70; //The legal velocitylimit is 70.
-  const distancePerPoint = 5; // The distance travelled per every point is 5km.
+function radarDevice(speed) {  
+    //This function is called radar device.
+  const velocityLimit = 70;
+   //The legal velocitylimit is 70.
+  const distancePerPoint = 5;
+   // The disance travelled per every point is 5km.
 
-  if (speed <= velocityLimit) {//This a conditional statement. it checks if the speed is <= the velocitylimit
-      console.log("Ok"); //If true it will display the string "ok".
+  if (speed <= velocityLimit) {
+    //This a conditional statement. it checks if the speed is <= the velocitylimit
+      console.log("Ok"); 
+      //If true it will display the string "ok".
   } else if (speed > velocityLimit) {
       // If the speed is greater than the velocitylimit,velocitylimit is subtracted from the speedlimit and divided by the distanceperpoint.
       let points = ((speed - velocityLimit) / distancePerPoint);
 
       if (points > 12) {
-          console.log("License suspended"); //This checks for points exceeded and is > than 12, it displays licence suspended.
+          console.log("License suspended"); 
+          //This checks for points exceeded and is > than 12, it displays licence suspended.
       } else {
-          console.log(`Points: ${points}`);//If points are not exceeded,it displays the value in the points container.
+          console.log(`Points: ${points}`);
+          //If points are not exceeded,it displays the value in the points container.
       }
   }
 }
